edb6fdf14627dd7034b9171535b95f42a2591601
myusername <manelbcsm1@gmail.com>
2019-11-09 17:16:37 +0000

    [Docs] Adding comments to the route apis

--- web/backend/src/models/Room.js
+++ web/backend/src/models/Room.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var Room = new Schema({
     name: {type: String, unique: true},
+    maxOccupation: {type: Number, default: 0},
     talk: [{type: Schema.Types.ObjectId, ref: 'Talk'}]
 });
 
