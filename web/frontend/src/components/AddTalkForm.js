37fe66b15e5e262a877648bc1b450f230a61a618
Simawatt <simaoxo@hotmail.com>
2019-12-02 18:59:41 +0000

    Visual part is somewhat done. does NOT post to db

--- web/frontend/src/components/AddTalkForm.js
+++ web/frontend/src/components/AddTalkForm.js
@@ -1,57 +1,22 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import axios from 'axios';
-import CloseButton from '@material-ui/core/Button';
 
 export default class AddTalkForm extends React.Component {
     constructor(props) {
       super(props);
       const { setOpen } = props;
 
-      this.state = {title: '',
-                    speaker: '',
-                    date: '',
-                    start: '',
-                    end: '',
-                    room: ''};
-  
-      this.handleTitle = this.handleTitle.bind(this);
-      this.handleSpeaker = this.handleSpeaker.bind(this);
-      this.handleDate = this.handleDate.bind(this);
-      this.handleStart = this.handleStart.bind(this);
-      this.handleEnd = this.handleEnd.bind(this);
-      this.handleRoom = this.handleRoom.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-
       this.handleClose = () => {
         setOpen(false);
       };
     }
   
-    handleTitle(event) {
-      this.setState({title: event.target.value});
-    }
-    handleSpeaker(event) {
-      this.setState({speaker: event.target.value});
-    }
-    handleDate(event) {
-      let date_s = new Date(event.target.value).getTime()/1000;
-      this.setState({date: date_s});
-    }
-    handleStart(event) {
-      let start_time = event.target.value;
-      let aux = start_time.split(':');
-      start_time = (aux[0] * 3600 + aux[1] * 60 + this.state.date)*1000;
-      this.setState({start: start_time});
-    }
-    handleEnd(event) {
-      let end_time = event.target.value;
-      let aux = end_time.split(':');
-      end_time = (aux[0] * 3600 + aux[1] * 60 + this.state.date)*1000;
-      this.setState({end: end_time});
-    }
-    handleRoom(event) {
-      this.setState({room: event.target.value});
-    }
 
     addTalkToDataBase(){
       let params = {
@@ -79,39 +44,26 @@ export default class AddTalkForm extends React.Component {
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit} >
-          <CloseButton className="close-button" onClick={this.handleClose} >
-            <legend>X</legend>
-          </CloseButton>
-          <fieldset>
-            <legend>Add a talk</legend>
-            <label>
-              Title:
-              <input type="text" title={this.state.title} onChange={this.handleTitle}/>
-            </label>
-            <label>
-              Speaker:
-              <input type="text" speaker={this.state.speaker} onChange={this.handleSpeaker}/>
-            </label>
-            <label>
-              Date:
-              <input type="date" date={this.state.date} onChange={this.handleDate}/>
-            </label>
-            <label>
-              Start:
-              <input type="time" start={this.state.start} onChange={this.handleStart}/>
-            </label>
-            <label>
-              End:
-              <input type="time" end={this.state.end} onChange={this.handleEnd}/>
-            </label>
-            <label>
-              Room:
-              <input type="text" room={this.state.room} onChange={this.handleRoom}/>
-            </label>
-            <input type="submit" value="Submit" />
-          </fieldset>
-        </form>
+          <Dialog open={true} onClose={this.handleClose} aria-labelledby="form-diaglog-title">
+            <DialogTitle id="fomr-dialog-title">
+              Add a Talk
+              </DialogTitle>
+            <DialogContent>
+            <form onSubmit={this.handleSubmit}>
+              <TextField autoFocus margin="dense" id="Title" label="Title" type="string" fullWidth />
+              <TextField margin="dense" id="Speaker" label="Speaker" type="string" fullWidth />
+              <TextField margin="dense" id="Date" label="Date" type="date" fullWidth />
+              <TextField margin="dense" id="Start" label="Start" type="time" fullWidth />
+              <TextField margin="dense" id="End" label="End" type="time" fullWidth />
+              <TextField margin="dense" id="Room" label="Room" type="string" fullWidth />
+            </form>
+            </DialogContent>
+            <DialogActions>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </DialogActions>
+          </Dialog>
       );
     }
 }
