836751d04f1edba7ca090ead2e934e9959146d15
Nuno <up201706162@fe.up.pt>
2019-12-06 09:48:13 +0000

    Changed to look good with material UI

--- web/frontend/src/components/AdminKey.js
+++ web/frontend/src/components/AdminKey.js
@@ -1,45 +1,57 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import CloseButton from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 
 export default class AdminKey extends React.Component {
-    constructor(props) {
-      super(props);
-      const { setOpenAdmin } = props;
+  constructor(props) {
+    super(props);
+    const { setOpenAdmin } = props;
 
-      this.state = {key: ''};
-  
-      this.handleKey = this.handleKey.bind(this);
+    this.state = { key: '',
+                   openAdmin: false};
 
-      this.handleClose = () => {
-        setOpenAdmin(false);
-      };
+    this.handleKey = this.handleKey.bind(this);
 
-      this.handleSubmit = event => {
-        event.preventDefault();
-        this.handleClose();
-      }
+    this.handleClose = () => {
+      setOpenAdmin(false);
+    };
+
+    this.handleSubmit = event => {
+      event.preventDefault();
+      this.handleClose();
     }
-  
-    handleKey(event) {
-      this.setState({key: event.target.value});
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit} >
-          <CloseButton className="close-button" onClick={this.handleClose} >
-            X
-          </CloseButton>
-          <fieldset className="login-form">
-            <legend>Insert admin key</legend>
-            <TextField onChange={this.handleKey} id="key_text" label="Key" />
-            <Button className="submit-button" variant="contained" color="primary" onClick={this.handleSubmit}>
-                Submit
-            </Button>
-          </fieldset>
-        </form>
+  }
+
+  handleKey(event) {
+    this.setState({ key: event.target.value });
+  }
+
+  render() {
+    return (
+      <Dialog open={true} onClose={this.handleClose} aria-labelledby="form-diaglog-title">
+          <form onSubmit={this.handleSubmit}>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="normal"
+                required={true}
+                id="Key" label="Key"
+                value={this.key} onChange={this.handleKey}
+                type="string" fullWidth
+              />
+              <DialogActions>
+                <Button type="submit" label="submit" color="primary">
+                  Submit
+                </Button>
+              </DialogActions>
+            </DialogContent>
+          </form>
+      </Dialog>
       );
-    }
+  }
 }
